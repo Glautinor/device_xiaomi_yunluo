c17df4bca7a5a7b346f6692cc1f8018d7a291130
Glautinor <Glautinor@Gmail.com>
2025-07-21 05:52:06 +0000

    welcome to voltage os

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,7 +115,7 @@ BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
 BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
 
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/voltage/config/BoardConfigReservedSize.mk
 
 TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_PRODUCT := product
@@ -171,7 +171,7 @@ DEVICE_MATRIX_FILE := $(CONFIGS_PATH)/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(CONFIGS_PATH)/vintf/device_framework_matrix.xml
 
 # VNDK
 BOARD_VNDK_VERSION := current
--- lineage_yunluo.mk
+++ lineage_yunluo.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/xiaomi/yunluo/device.mk)
 
 # Inherit some common LineageOS Stuff
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/voltage/config/common_full_tablet_wifionly.mk)
 
 # Disable Eppe
 TARGET_DISABLE_EPPE := true
