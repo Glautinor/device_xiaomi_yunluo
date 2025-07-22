d3f826b874c3746b17af96924204f74601fbe1fc
Glautinor <Glautinor@Gmail.com>
2025-07-22 16:48:32 +0000

    Welcome to rising os

--- lineage_yunluo.mk
+++ lineage_yunluo.mk
@@ -29,3 +29,37 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BuildDesc="missi_pad_global-user 14 UP1A.231005.007 V816.0.9.0.ULYMIXM release-keys" \
   BuildFingerprint=Redmi/yunluo_global/yunluo:12/SP1A.210812.016/V816.0.9.0.ULYMIXM:user/release-keys
 
+#RisingOS-Revived
+# Lunch banner maintainer variable
+RISING_MAINTAINER="@Boy_Sid"
+
+# Chipset/Maintainer properties (ro.rising.chipset/ro.rising.maintainer)
+# Set RISING_MAINTAINER for version control
+# (Optional if builder is setting properties via init_<device>.cpp)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Mediatek Helio G99" \
+    RisingMaintainer="@Boy_Sid"
+
+RISING_MAINTAINER := @Boy_Sid
+
+# Disable/enable blur support, false by default
+TARGET_ENABLE_BLUR := true
+
+# Whether to ship aperture camera, false by default
+PRODUCT_NO_CAMERA := false
+
+# Whether to ship lawnchair launcher, false by default
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# GMS build flags, true by default
+# Ship with GMS packages, replaces default AOSP packages with Google manufactured packages.
+WITH_GMS := true
+
+# CORE build flags
+#TARGET_USES_PICO_GAPPS := true
+
+# These flags needs WITH_GMS set to true
+# Whether to ship pixel launcher and set it as default launcher, false by default
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+
+#END
